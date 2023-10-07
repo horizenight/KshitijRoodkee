ff5c1eb7af720e6d48a926d53e0df24c1229df42
horizenight <kshitijroodkee1@gmail.com>
2023-10-07 16:25:35 +0530

    [fix]: portfolio updated
    Signed-off-by: horizenight <kshitijroodkee1@gmail.com>

---
+++ src/assets/index.js
@@ -0,0 +1,74 @@
+import logo from "./logo.svg";
+import backend from "./backend.png";
+import creator from "./creator.png";
+import mobile from "./mobile.png";
+import web from "./web.png";
+import github from "./github.png";
+import menu from "./menu.svg";
+import close from "./close.svg";
+
+import css from "./tech/css.png";
+import docker from "./tech/docker.png";
+import figma from "./tech/figma.png";
+import git from "./tech/git.png";
+import html from "./tech/html.png";
+import javascript from "./tech/javascript.png";
+import mongodb from "./tech/mongodb.png";
+import nodejs from "./tech/nodejs.png";
+import reactjs from "./tech/reactjs.png";
+import redux from "./tech/redux.png";
+import tailwind from "./tech/tailwind.png";
+import typescript from "./tech/typescript.png";
+import threejs from "./tech/threejs.svg";
+
+import nimbus from './company/Nimbus2k22.png'
+
+import meta from "./company/meta.png";
+import shopify from "./company/shopify.png";
+import starbucks from "./company/starbucks.png";
+import tesla from "./company/tesla.png";
+
+import carrent from "./carrent.png";
+import jobit from "./jobit.png";
+import tripguide from "./tripguide.png";
+
+export {
+  logo,
+  backend,
+  creator,
+  mobile,
+  web,
+  github,
+  menu,
+  close,
+  css,
+  docker,
+  figma,
+  git,
+  html,
+  javascript,
+  mongodb,
+  nodejs,
+  reactjs,
+  redux,
+  tailwind,
+  typescript,
+  threejs,
+  meta,
+  shopify,
+  starbucks,
+  tesla,
+  carrent,
+  jobit,
+  tripguide,
+  nimbus,
+
+};
+
+
+export const ncbsLogo = "https://www.ncbs.res.in/sites/default/files/NCBS-logo.png"
+export const pharmScrollLogo = "https://media.licdn.com/dms/image/D4D0BAQHWWiV_8cyGOw/company-logo_200_200/0/1683892299673?e=1704931200&v=beta&t=KfIn80PXPO7z3CN-RxJdaHhDkIHZREBp9Jq2H6olwS0"
+export const asiaTechLogo = "https://media.licdn.com/dms/image/C4D0BAQEUaU1Bt4LQwA/company-logo_200_200/0/1650823098844?e=1704931200&v=beta&t=m2GmmaJkrCAPZLP57E0W-sCoB9jij9wL_P2xT0qsqRw"
+export const dwDLogo = "https://mainstack.me/_next/image?url=https%3A%2F%2Fmedia.mainstack.me%2Fimages%2F1672783083962.webp&w=1920&q=75"
+export const DPS = "https://media.licdn.com/dms/image/C4D0BAQFfVj8pfxtq3g/company-logo_100_100/0/1611070994655?e=1704931200&v=beta&t=6-sXFVXVPiF0n4vB_ETYbV7lRG0aQJ46kRCBr-J7eiI"
+export const HyperLedger = "https://media.licdn.com/dms/image/D560BAQH5Em7J2Jsaig/company-logo_100_100/0/1690943279876?e=1704931200&v=beta&t=Yi3HJPWPCN00AomMgAx-tR8MIE_s39R2CCx6VnxAXTw"
